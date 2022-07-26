10ab2fa03b91ac23a01c752800e1aca527ee7943
camhar28 <100096993+camhar28@users.noreply.github.com>
2022-07-26 14:56:31 +1200

    Day Whatever number

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 export (int) var speed = 120
-export (int) var jump_speed = -180
+export (int) var jump_speed = -180    
 export (int) var gravity = 400
 export (int) var slide_speed = 400
 
@@ -10,7 +10,7 @@ var velocity = Vector2.ZERO
 export (float) var friction = 10
 export (float) var acceleration = 25
 
-enum state {IDLE, RUNNINIG, PUSHING, ROLLING, JUMP, STARTJUMP, FALL, ATTACK}
+enum state {IDLE, RUNNING, PUSHING, ROLLING, JUMP, STARTJUMP, FALL, ATTACK}
 
 onready var player_state = state.IDLE
 
@@ -18,10 +18,32 @@ func _ready():
 	$AnimationPlayer.play("Idle")
 	pass
 
-func update_animation(_anim):
-	pass
-	
-func handle_state(_state):
+func update_animation(anim):
+	if velocity.x < 0:
+		$Sprite.flip_h = true
+	elif velocity.x > 0:
+		$Sprite.flip_h = false
+
+	match(anim):
+		state.FALL:
+			$AnimationPlayer.play("Fall")
+		state.ATTACK:
+			$AnimationPlayer.play("Attack")
+		state.IDLE:
+			$AnimationPlayer.play("Idle")
+		state.JUMP:
+			$AnimationPlayer.play("Jump")
+		state.PUSHING:
+			$AnimationPlayer.play("Pushing")
+		state.RUNNING:
+			$AnimationPlayer.play("Running")
+			pass
+
+# warning-ignore:shadowed_variable
+func handle_state(player_state):
+	match(player_state):
+		state.STARTJUMP:
+			velocity.y = jump_speed
 	pass
 
 func get_input():
@@ -30,23 +52,25 @@ func get_input():
 		velocity.x = move_toward(velocity.x, dir*speed, acceleration)
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction)
-	
-	
+
 func _physics_process(delta):
 	get_input()
+	print(is_on_floor())
 	if velocity == Vector2.ZERO:
 		player_state = state.IDLE
-	elif velocity.x != 0 and Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		player_state = state.STARTJUMP
 	elif velocity.x != 0:
-		player_state = state.RUNNINIG 
-		
+		player_state = state.RUNNING
+
 	if not is_on_floor():
 		if velocity.y < 0:
 			player_state = state.JUMP
 		if velocity.y > 0:
 			player_state = state.FALL
-			
+
+	handle_state(player_state)
+	update_animation(player_state)
 	#set gravity
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
